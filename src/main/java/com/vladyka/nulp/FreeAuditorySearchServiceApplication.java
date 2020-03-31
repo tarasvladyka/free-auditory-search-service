878f3f0a8a99eda7cb603f226a449424e065eabf
Taras <tarasvladyka@gmail.com>
2020-04-01 00:10:09 +0300

    Initial commit

---
+++ src/main/java/com/vladyka/nulp/FreeAuditorySearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vladyka.nulp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeAuditorySearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeAuditorySearchServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vladyka/nulp/FreeAuditorySearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladyka.nulp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeAuditorySearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
